22484b2ce9528c4808ac9fe7fbc6ca04b233962c
Dillon <dillon@163.com>
2019-10-11 14:18:46 +0800

    2019-10-11 RabbitMQ简单通信

---
+++ RabbitMQDemo/src/main/java/com/wudi/rabbit/RabbitMQApplication.java
@@ -0,0 +1,16 @@
+package com.wudi.rabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class RabbitMQApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMQApplication.class, args);
+	}
+
+
+
+}
